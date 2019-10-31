dcc196f827c013ec516ca5223eaf5fc0d83cea01
EqualMa <equalma@outlook.com>
2019-10-31 11:30:43 +0000

    WIP(js) setting up rollup

--- packages/js/rollup.config.js
+++ packages/js/rollup.config.js
@@ -1,20 +1,26 @@
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import pkg from "./package.json";
+import typescript from "rollup-plugin-typescript2";
+
+const input = "src/index.ts";
 
 export default [
   // browser-friendly UMD build
   {
-    input: "src/main.js",
+    input,
     output: {
       name: "howLongUntilLunch",
       file: pkg.browser,
-      format: "umd"
+      format: "umd",
     },
     plugins: [
-      resolve(), // so Rollup can find `ms`
-      commonjs() // so Rollup can convert `ms` to an ES module
-    ]
+      resolve(), // so Rollup can find packages in node_modules
+      typescript({
+        tsconfig: "./tsconfig.prod.json",
+      }),
+      commonjs(), // so Rollup can convert packages in node_modules to an ES module
+    ],
   },
 
   // CommonJS (for Node) and ES module (for bundlers) build.
@@ -24,11 +30,12 @@ export default [
   // an array for the `output` option, where we can specify
   // `file` and `format` for each target)
   {
-    input: "src/main.js",
-    external: ["ms"],
+    input,
+    // TODO: auto-external
+    external: [],
     output: [
       { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "es" }
-    ]
-  }
+      { file: pkg.module, format: "es" },
+    ],
+  },
 ];
