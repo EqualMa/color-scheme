14b1639dfeb338c5eb4576203e0907ac518ea26b
EqualMa <equalma@outlook.com>
2019-12-08 02:37:12 +0000

    WIP(browserslist-config) common browserslist config

---
+++ tools/browserslist-config/index.js
@@ -0,0 +1,11 @@
+module.exports = [
+  "and_chr >= 78",
+  "and_ff >= 68",
+  "android >= 76",
+  "chrome >= 76",
+  "edge >= 76",
+  "firefox >= 67",
+  "ios_saf >= 13",
+  "opera >= 62",
+  "safari >= 12.1"
+];
